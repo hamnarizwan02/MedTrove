5953076f1d8744df5f6687f5f5cb6831661d6b66
Aliza-03 <alizaibm3000@gmail.com>
2024-10-05 22:04:11 +0500

    adding navigation to profile page

--- MedTrove/App.js
+++ MedTrove/App.js
@@ -7,20 +7,23 @@ import Splash from './splash';
 import Search from './search';
 import MedicineDetails from './medinfo';
 import ProductList from './productList';
+import ProfileManagement from './profilemangement';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Splash">
+      <Stack.Navigator initialRouteName="Search">
       <Stack.Screen name="Splash" component={Splash} options={{ headerShown: false }} />
         <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
         <Stack.Screen name="SignUp" component={SignUp} options={{ headerShown: false }} />
         <Stack.Screen name="Search" component={Search} options={{ headerShown: false }} />
         <Stack.Screen name="ProductList" component={ProductList} options={{ headerShown: false }} />
         <Stack.Screen name="MedInfo" component={MedicineDetails} options={{ headerShown: false }} /> 
+        <Stack.Screen name="ProfileManagement" component={ProfileManagement} options={{ headerShown: false }} />
+     
       </Stack.Navigator>
     </NavigationContainer>
-  );
+    );
 }
